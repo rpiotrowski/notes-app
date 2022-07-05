348dafd0dbcabd03e5ac12fa240fe87ecba8bd26
rpiotrowski <rafcikp2@gmail.com>
2022-07-05 23:07:19 +0200

    Add new route and serializer for Note model
    In order to present it in json

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from api import views
 
+# Define urls and connect it with views
 urlpatterns = [
     path('', views.get_routes, name="routes"),
     path('notes/', views.get_notes, name="notes"),
--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from .models import Note
+from .serializers import NoteSerializer
 
 
 @api_view(['GET'])
@@ -43,5 +44,7 @@ def get_routes(request):
 
 @api_view(['GET'])
 def get_notes(request):
+    notes = Note.objects.all()
+    serializer = NoteSerializer(notes, many=True)
 
-    return Response('NOTES')
+    return Response(serializer.data)
